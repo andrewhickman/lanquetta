e128aa65dd8592382d59cc2a4d4097fadf7024da
Andrew Hickman <andrew.hickman1@sky.com>
2020-10-17 13:54:28 +0100

    Convert method to composition

--- src/app/sidebar/method.rs
+++ src/app/sidebar/method.rs
@@ -1,6 +1,4 @@
-use druid::{
-    widget::prelude::*, widget::Label, ArcStr, Data, FontDescriptor, FontFamily, Lens, WidgetExt,
-};
+use druid::{ArcStr, Data, FontDescriptor, FontFamily, Lens, RenderContext, Widget, WidgetExt, widget::Label, widget::Painter};
 
 use crate::{app::command, protobuf::ProtobufMethod, theme};
 
@@ -15,21 +13,29 @@ pub(in crate::app) struct MethodState {
     method: ProtobufMethod,
 }
 
-struct Method {
-    label: Box<dyn Widget<State>>,
-}
-
-pub(in crate::app) fn build() -> impl Widget<State> {
-    Method {
-        label: Label::raw()
-            .with_font(FontDescriptor::new(FontFamily::SANS_SERIF))
-            .with_text_size(16.0)
-            .padding((theme::GUTTER_SIZE, theme::GUTTER_SIZE / 4.0))
-            .expand_width()
-            .lens(MethodState::name())
-            .lens(State::method)
-            .boxed(),
-    }
+pub(in crate::app) fn build() -> Box<dyn Widget<State>> {
+    Label::raw()
+        .with_font(FontDescriptor::new(FontFamily::SANS_SERIF))
+        .with_text_size(16.0)
+        .padding((theme::GUTTER_SIZE, theme::GUTTER_SIZE / 4.0))
+        .expand_width()
+        .lens(MethodState::name())
+        .lens(State::method)
+        .background(Painter::new(|ctx, data: &State, env| {
+            let mut color = env.get(theme::SIDEBAR_BACKGROUND);
+            if ctx.is_active() || data.selected {
+                color = theme::color::active(color, env.get(druid::theme::LABEL_COLOR));
+            } else if ctx.is_hot() {
+                color = theme::color::hot(color, env.get(druid::theme::LABEL_COLOR));
+            }
+            let bounds = ctx.size().to_rect();
+            ctx.fill(bounds, &color);
+        }))
+        .on_click(|ctx, data, _|{
+            data.selected = true;
+            ctx.submit_command(command::SELECT_METHOD.with(data.method.method.clone()));
+        })
+        .boxed()
 }
 
 impl State {
@@ -63,65 +69,5 @@ impl MethodState {
 impl From<ProtobufMethod> for MethodState {
     fn from(method: ProtobufMethod) -> Self {
         MethodState { method }
-    }
-}
-
-impl Widget<State> for Method {
-    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut State, _: &Env) {
-        match event {
-            Event::MouseDown(_) => {
-                ctx.set_active(true);
-                ctx.request_paint();
-            }
-            Event::MouseUp(_) => {
-                if ctx.is_active() {
-                    if ctx.is_hot() {
-                        data.selected = true;
-                        ctx.submit_command(command::SELECT_METHOD.with(data.method.method.clone()));
-                    }
-
-                    ctx.set_active(false);
-                    ctx.request_paint();
-                }
-            }
-            _ => (),
-        }
-    }
-
-    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &State, env: &Env) {
-        if let LifeCycle::HotChanged(_) = event {
-            ctx.request_paint();
-        }
-        self.label.lifecycle(ctx, event, data, env)
-    }
-
-    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &State, data: &State, env: &Env) {
-        self.label.update(ctx, old_data, data, env);
-        if old_data.selected != data.selected {
-            ctx.request_paint()
-        }
-    }
-
-    fn layout(
-        &mut self,
-        ctx: &mut LayoutCtx,
-        bc: &BoxConstraints,
-        data: &State,
-        env: &Env,
-    ) -> druid::Size {
-        self.label.layout(ctx, bc, data, env)
-    }
-
-    fn paint(&mut self, ctx: &mut PaintCtx, data: &State, env: &Env) {
-        let mut color = env.get(theme::SIDEBAR_BACKGROUND);
-        if ctx.is_active() || data.selected {
-            color = theme::color::active(color, env.get(druid::theme::LABEL_COLOR));
-        } else if ctx.is_hot() {
-            color = theme::color::hot(color, env.get(druid::theme::LABEL_COLOR));
-        }
-        let bounds = ctx.size().to_rect();
-        ctx.fill(bounds, &color);
-
-        self.label.paint(ctx, data, env)
     }
 }
