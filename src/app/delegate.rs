491d0fa68cccce38aec3de436510531b3f4cafce
Andrew Hickman <andrew.hickman1@sky.com>
2020-10-18 15:05:55 +0100

    Update

--- src/app/delegate.rs
+++ src/app/delegate.rs
@@ -1,4 +1,4 @@
-use druid::{AppDelegate, ExtEventSink, SingleUse, Target};
+use druid::{AppDelegate, ExtEventSink, SingleUse, Target, DelegateCtx, Command, Env, Handled};
 
 use crate::app::{self, command};
 use crate::grpc;
@@ -18,18 +18,18 @@ struct Delegate {
 impl AppDelegate<app::State> for Delegate {
     fn command(
         &mut self,
-        _ctx: &mut druid::DelegateCtx,
-        _target: druid::Target,
-        cmd: &druid::Command,
+        _ctx: &mut DelegateCtx,
+        _target: Target,
+        cmd: &Command,
         data: &mut app::State,
-        _env: &druid::Env,
-    ) -> bool {
+        _env: &Env,
+    ) -> Handled {
         log::info!("Received command: {:?}", cmd);
         if let Some(file) = cmd.get(druid::commands::OPEN_FILE) {
             if let Err(err) = data.sidebar.add_from_path(file.path()) {
                 log::error!("Error loading file: {:?}", err);
             }
-            false
+            Handled::Yes
         } else if cmd.is(command::START_SEND) {
             let event_sink = self.event_sink.clone();
             self.grpc_client
@@ -42,14 +42,14 @@ impl AppDelegate<app::State> for Delegate {
                         )
                         .ok();
                 });
-            false
+            Handled::Yes
         } else if let Some(response) = cmd.get(command::FINISH_SEND) {
             // TODO how do we identify the tab to set the response on?
             let result = response.take().expect("response already handled");
             data.body.response.update(result);
-            false
+            Handled::Yes
         } else {
-            true
+            Handled::No
         }
     }
 }
