424b5bab86730fec7d338e88b3f5f4b3d339136f
Andrew Hickman <andrew.hickman1@sky.com>
2020-10-26 21:26:47 +0000

    remove tls checkbox

--- src/app/body/address.rs
+++ src/app/body/address.rs
@@ -1,7 +1,7 @@
 use std::string::ToString;
 use std::{str::FromStr, sync::Arc};
 
-use druid::widget::{Button, Checkbox, Flex, TextBox};
+use druid::widget::{Button, Flex, TextBox};
 use druid::{Data, Env, EventCtx, Lens, Target, Widget, WidgetExt as _};
 use http::Uri;
 use once_cell::sync::Lazy;
@@ -17,11 +17,9 @@ pub(in crate::app) struct State {
 
 pub(in crate::app) fn build() -> Box<dyn Widget<State>> {
     let address_textbox = TextBox::new()
-        .with_placeholder("localhost:80")
+        .with_placeholder("http://localhost:80")
         .expand_width();
     let address_form_field = FormField::new(address_textbox);
-    let tls_checkbox = Checkbox::new("Use TLS")
-        .env_scope(|env, _| env.set(druid::theme::BORDER_LIGHT, theme::color::SUBTLE_ACCENT));
     let send_button = theme::scope(Button::new("Send").on_click(
         |ctx: &mut EventCtx, _: &mut State, _: &Env| {
             ctx.submit_command(command::START_SEND.to(Target::Global))
@@ -30,8 +28,6 @@ pub(in crate::app) fn build() -> Box<dyn Widget<State>> {
 
     Flex::row()
         .with_flex_child(address_form_field.lens(State::address), 1.0)
-        .with_spacer(theme::GUTTER_SIZE)
-        .with_child(tls_checkbox.lens(State::tls))
         .with_spacer(theme::GUTTER_SIZE)
         .with_child(send_button)
         .boxed()
