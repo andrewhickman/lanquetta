0c2335a14ae50cf981451186d078007532ffdb27
Andrew Hickman <andrew.hickman1@sky.com>
2022-01-23 01:00:39 +0000

    Implement option to disable certificate validation
    Closes #13

--- src/grpc/channel.rs
+++ src/grpc/channel.rs
@@ -2,10 +2,10 @@ use std::sync::Arc;
 
 use dashmap::{mapref::entry::Entry, DashMap};
 use futures::future::BoxFuture;
-use http::Uri;
+use http::{uri::Scheme, Uri};
 use once_cell::sync::Lazy;
 use tokio::sync::Mutex;
-use tonic::transport::Channel;
+use tonic::transport::{Channel, ClientTlsConfig};
 
 use crate::grpc;
 
@@ -31,7 +31,7 @@ pub async fn get(uri: &Uri, verify_certs: bool) -> Result<Channel, grpc::Error> 
     let state = match CHANNELS.entry(key) {
         Entry::Occupied(entry) => entry.get().clone(),
         Entry::Vacant(entry) => {
-            let state = Arc::new(Mutex::new(ChannelState::new(uri.clone())));
+            let state = Arc::new(Mutex::new(ChannelState::new(uri.clone(), verify_certs)));
             entry.insert(Arc::clone(&state));
             state
         }
@@ -53,22 +53,76 @@ pub async fn get(uri: &Uri, verify_certs: bool) -> Result<Channel, grpc::Error> 
             }
             ChannelState::Ready(channel) => return Ok(channel.clone()),
             ChannelState::Error => {
-                *lock = ChannelState::new(uri.clone());
+                *lock = ChannelState::new(uri.clone(), verify_certs);
             }
         }
     }
 }
 
 impl ChannelState {
-    fn new(uri: Uri) -> Self {
-        ChannelState::Pending(Box::pin(connect(uri)))
+    fn new(uri: Uri, verify_certs: bool) -> Self {
+        ChannelState::Pending(Box::pin(connect(uri, verify_certs)))
     }
 }
 
-async fn connect(uri: Uri) -> Result<Channel, grpc::Error> {
-    Channel::builder(uri)
+const ALPN_H2: &[u8] = b"h2";
+
+async fn connect(uri: Uri, verify_certs: bool) -> Result<Channel, grpc::Error> {
+    let is_https = uri.scheme() == Some(&Scheme::HTTPS);
+    let mut builder = Channel::builder(uri);
+
+    if is_https {
+        let mut tls_config = ClientTlsConfig::new();
+        if !verify_certs {
+            let mut rustls_config = rustls::ClientConfig::new();
+            rustls_config.set_protocols(&[ALPN_H2.to_vec()]);
+            rustls_config
+                .dangerous()
+                .set_certificate_verifier(Arc::new(DangerousCertificateVerifier));
+            tls_config = tls_config.rustls_client_config(rustls_config);
+        }
+
+        builder = builder
+            .tls_config(tls_config)
+            .map_err(anyhow::Error::from)
+            .map_err(grpc::Error::from)?;
+    }
+
+    builder
         .connect()
         .await
         .map_err(anyhow::Error::from)
         .map_err(grpc::Error::from)
+}
+
+struct DangerousCertificateVerifier;
+
+impl rustls::ServerCertVerifier for DangerousCertificateVerifier {
+    fn verify_server_cert(
+        &self,
+        _roots: &rustls::RootCertStore,
+        _presented_certs: &[rustls::Certificate],
+        _dns_name: webpki::DNSNameRef,
+        _ocsp_response: &[u8],
+    ) -> Result<rustls::ServerCertVerified, rustls::TLSError> {
+        Ok(rustls::ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        _message: &[u8],
+        _cert: &rustls::Certificate,
+        _dss: &rustls::internal::msgs::handshake::DigitallySignedStruct,
+    ) -> Result<rustls::HandshakeSignatureValid, rustls::TLSError> {
+        Ok(rustls::HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        _message: &[u8],
+        _cert: &rustls::Certificate,
+        _dss: &rustls::internal::msgs::handshake::DigitallySignedStruct,
+    ) -> Result<rustls::HandshakeSignatureValid, rustls::TLSError> {
+        Ok(rustls::HandshakeSignatureValid::assertion())
+    }
 }
