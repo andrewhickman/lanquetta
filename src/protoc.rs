b24f4ae29582b409a815e08ff6448952d55e3732
Andrew Hickman <andrew.hickman1@sky.com>
2023-06-04 22:42:57 +0100

    Better construction of DescriptorPool

--- src/protoc.rs
+++ src/protoc.rs
@@ -5,14 +5,11 @@ use prost_reflect::DescriptorPool;
 
 pub fn load_file(path: &Path) -> Result<DescriptorPool> {
     match path.parent() {
-        Some(include) => Ok(DescriptorPool::decode(
-            protox::Compiler::new([include])?
-                .include_imports(true)
-                .include_source_info(false)
-                .open_file(path)?
-                .encode_file_descriptor_set()
-                .as_slice(),
-        )?),
+        Some(include) => Ok(protox::Compiler::new([include])?
+            .include_imports(true)
+            .include_source_info(false)
+            .open_file(path)?
+            .descriptor_pool()),
         None => bail!("invalid path"),
     }
 }
