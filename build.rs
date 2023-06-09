f1861f70bf7fbc9dd0d489ca18d14261402037ed
Andrew Hickman <andrew.hickman1@sky.com>
2023-06-09 23:25:48 +0100

    fmt

--- build.rs
+++ build.rs
@@ -2,7 +2,9 @@ fn main() -> anyhow::Result<()> {
     vergen::EmitBuilder::builder().git_sha(true).emit()?;
 
     #[cfg(windows)]
-    winres::WindowsResource::new().set_icon("img/logo.ico").compile()?;
+    winres::WindowsResource::new()
+        .set_icon("img/logo.ico")
+        .compile()?;
 
     Ok(())
 }
