cf02bb2b37f3008edc093d24a738fc4f48e8fbd2
Andrew Hickman <andrew.hickman1@sky.com>
2023-06-09 23:20:26 +0100

    Fix ci

--- build.rs
+++ build.rs
@@ -1,10 +1,8 @@
-use winres::WindowsResource;
-
 fn main() -> anyhow::Result<()> {
     vergen::EmitBuilder::builder().git_sha(true).emit()?;
 
     #[cfg(windows)]
-    WindowsResource::new().set_icon("img/logo.ico").compile()?;
+    winres::WindowsResource::new().set_icon("img/logo.ico").compile()?;
 
     Ok(())
 }
