428c957dc63d680060b473594d521a9a0f42853e
Andrew Hickman <andrew.hickman1@sky.com>
2023-06-17 01:32:46 +0100

    fmt

--- build.rs
+++ build.rs
@@ -3,7 +3,10 @@ fn main() -> anyhow::Result<()> {
 
     #[cfg(windows)]
     winres::WindowsResource::new()
-        .set_icon_with_id("img/logo.ico", &(windows::Win32::UI::WindowsAndMessaging::IDI_APPLICATION .0 as u32).to_string())
+        .set_icon_with_id(
+            "img/logo.ico",
+            &(windows::Win32::UI::WindowsAndMessaging::IDI_APPLICATION.0 as u32).to_string(),
+        )
         .compile()?;
 
     Ok(())
